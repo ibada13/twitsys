b3576b59d24f9e13042cd26c6f0023dcc323809a
hunter666hz <hzhunter7@gmail.com>
2024-02-15 12:18:21 +0100

    modified:   bk/myenv/Scripts/db.sqlite3 	modified:   bk/myenv/Scripts/tweet/urls.py 	modified:   bk/myenv/Scripts/tweets/models.py 	modified:   bk/myenv/Scripts/tweets/views.py
    	modified:   .gitignore
    	modified:   bk/myenv/Scripts/tweet/__pycache__/urls.cpython-312.pyc

--- bk/myenv/Scripts/tweet/urls.py
+++ bk/myenv/Scripts/tweet/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from tweets.views import home_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('homev/', home_views),
+    
 ]
--- bk/myenv/Scripts/tweets/views.py
+++ bk/myenv/Scripts/tweets/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home_views(req ,*args,**kwargs):
+    S = "<h1>hello from home views </h1> "
+    return HttpResponse("<h1>hello from home views </h1>"); 
+    
